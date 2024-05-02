9fe2dc510e95c2aea8410eadf55d291795feb054
Yangyang <161786242+yangyang26lee@users.noreply.github.com>
2024-05-02 12:12:24 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,23 @@
+let Distance = 0
 basic.forever(function () {
-    pins.digitalWritePin(DigitalPin.P8, 1)
-    music.play(music.tonePlayable(392, music.beat(BeatFraction.Whole)), music.PlaybackMode.InBackground)
-    basic.pause(500)
-    pins.digitalWritePin(DigitalPin.P8, 0)
-    pins.digitalWritePin(DigitalPin.P13, 1)
-    music.play(music.tonePlayable(220, music.beat(BeatFraction.Whole)), music.PlaybackMode.InBackground)
-    basic.pause(500)
-    pins.digitalWritePin(DigitalPin.P13, 0)
+    Distance = sonar.ping(
+    DigitalPin.P16,
+    DigitalPin.P14,
+    PingUnit.Centimeters
+    )
+    basic.showNumber(Distance)
+})
+basic.forever(function () {
+    if (Distance < 10) {
+        pins.digitalWritePin(DigitalPin.P8, 1)
+        music.play(music.tonePlayable(392, music.beat(BeatFraction.Whole)), music.PlaybackMode.InBackground)
+        basic.pause(500)
+        pins.digitalWritePin(DigitalPin.P8, 0)
+        pins.digitalWritePin(DigitalPin.P13, 1)
+        music.play(music.tonePlayable(220, music.beat(BeatFraction.Whole)), music.PlaybackMode.InBackground)
+        basic.pause(500)
+        pins.digitalWritePin(DigitalPin.P13, 0)
+    } else {
+        music.stopAllSounds()
+    }
 })
